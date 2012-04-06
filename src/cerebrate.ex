2e8d4217369c6ab33b83d9a5c77abe177f2d0f5a
Carlo Cabanilla <carlo.cabanilla@gmail.com>
2012-04-06 00:10:28 -0400

    add a simple cowboy http listener to display data queried from the collector

--- src/cerebrate.ex
+++ src/cerebrate.ex
@@ -3,11 +3,19 @@ defmodule :cerebrate do
 
   def start() do
     Erlang.application.start :dnssd
+    Erlang.application.start :cowboy
     Erlang.application.start :cerebrate
   end
 
   def start(_type, _args) do
     IO.puts "Starting cerebrate"
+    dispatch = [
+      {:'_', [{:'_', CerebrateWeb, []}]}
+    ] 
+    Erlang.cowboy.start_listener(:cerebrate_http_listener, 100,
+      :cowboy_tcp_transport, [{:port, 8080}],
+      :cowboy_http_protocol, [{:dispatch, dispatch}]
+    )
     Cerebrate.Supervisor.start_link()
   end
 
@@ -34,4 +42,31 @@ defmodule Cerebrate do
       ]}}
     end
   end
+end
+
+defmodule CerebrateWeb do
+  @behavior :cowboy_http_handler
+
+  def init({_, :http}, req, []) do
+    {:ok, req, :undefined}
+  end
+
+  def handle(req, state) do
+    Process.whereis(:collector) <- {:query, Process.self()}
+    {:ok, req2} = receive do
+    match: data
+      output = Enum.map data, fn({metric, value}) ->  
+        [metric, float_to_list(value)]
+      end
+      Erlang.cowboy_http_req.reply(200, [], ["Data:", output], req)
+    after: 5000
+      Erlang.cowboy_http_req.reply(500, [], "Timed out", req)
+    end
+    {:ok, req2, state}
+  end
+
+  def terminate(_, _) do
+    :ok
+  end
+
 end
