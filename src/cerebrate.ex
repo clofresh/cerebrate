9e88a69851004b8684362e64af57c51f98f77c8a
Carlo Cabanilla <carlo@datadoghq.com>
2012-04-15 11:31:07 -0400

    split web module into its own file

--- src/cerebrate.ex
+++ src/cerebrate.ex
@@ -65,25 +65,3 @@ defmodule CerebrateSupervisor do
   end
 end
 
-defmodule CerebrateWeb do
-  @behavior :cowboy_http_handler
-
-  def init({_, :http}, req, [_config]) do
-    {:ok, req, :undefined}
-  end
-
-  def handle(req, state) do
-    data = CerebrateRpc.check_data
-    IO.inspect data
-    reply = ["Data:", data]
-    ExLog.info "replying with #{reply}"
-    {:ok, req2} = Erlang.cowboy_http_req.reply(200, [], reply, req)
-
-    {:ok, req2, state}
-  end
-
-  def terminate(_, _) do
-    :ok
-  end
-
-end
