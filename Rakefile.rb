1bd902b31c1a5bd45562cba94d5c8f02a7a7a51a
Carlo Cabanilla <carlo@datadoghq.com>
2012-04-15 22:46:56 -0400

    let rebar generate the app file

--- Rakefile.rb
+++ Rakefile.rb
@@ -19,6 +19,5 @@ task :run, :rpc_port, :web_port do |task, args|
 	rpc_port = args.rpc_port || 3456
 	web_port = args.web_port || 8080
 	ENV["PATH"] += ":#{Dir.pwd}/deps/elixir/bin"
-	sh "cp src/cerebrate.app.src ebin/__MAIN__.Cerebrate.app"
 	sh "./bin/cerebrate --rpc_port #{rpc_port} --web_port #{web_port}"
 end
